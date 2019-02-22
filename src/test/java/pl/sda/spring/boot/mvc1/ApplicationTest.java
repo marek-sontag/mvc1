95978728ca1347404070dc2219f52e63d9ccd42f
Marek Sontag <mareksontag@gmail.com>
2019-02-22 18:02:25 +0100

    GreetingService (Zad 6.6 )

--- src/test/java/pl/sda/spring/boot/mvc1/ApplicationTest.java
+++ src/test/java/pl/sda/spring/boot/mvc1/ApplicationTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -20,9 +22,19 @@ public class ApplicationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private GreetingService service;
+
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
         this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello, World!")));
+    }
+
+    @Test
+    public void shouldReturnGreetingFromService() throws Exception {
+        when(service.greeting()).thenReturn("Hello Mock");
+        this.mockMvc.perform(get("/greetingService")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello Mock")));
     }
 }
