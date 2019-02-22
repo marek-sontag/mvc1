f5b5e82da3a69f463f9d4d70bfd1e17e140b171d
Marek Sontag <mareksontag@gmail.com>
2019-02-22 17:40:18 +0100

    Test with MockMvc web only (Zad 6.4)

--- src/test/java/pl/sda/spring/boot/mvc1/ApplicationTest.java
+++ src/test/java/pl/sda/spring/boot/mvc1/ApplicationTest.java
@@ -3,8 +3,7 @@ package pl.sda.spring.boot.mvc1;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,8 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest
 public class ApplicationTest {
 
     @Autowired
