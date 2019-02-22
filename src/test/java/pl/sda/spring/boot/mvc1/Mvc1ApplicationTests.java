7e3cb2d81858c554f923be7181514d722cdf8e60
Marek Sontag <mareksontag@gmail.com>
2019-02-22 17:29:54 +0100

    Testing context load (Zad 6.1)

--- src/test/java/pl/sda/spring/boot/mvc1/Mvc1ApplicationTests.java
+++ src/test/java/pl/sda/spring/boot/mvc1/Mvc1ApplicationTests.java
@@ -2,15 +2,22 @@ package pl.sda.spring.boot.mvc1;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Mvc1ApplicationTests {
 
+	@Autowired
+	private GreetingController controller;
+
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
